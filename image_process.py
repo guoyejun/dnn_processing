3b14ed1c160421efc344786cb9ecf4032d884deb
Guo, Yejun <yejun.guo@intel.com>
2020-04-11 13:04:47 +0800

    change image_process.py to add 'add'

--- image_process.py
+++ image_process.py
@@ -6,10 +6,13 @@ in_img = imageio.imread('input.jpg')
 in_img = in_img.astype(np.float32)/255.0
 in_data = in_img[np.newaxis, :]
 
-filter_data = np.array([0.5, 0, 0, 0, 1., 0, 0, 0, 1.]).reshape(1,1,3,3).astype(np.float32)
-filter = tf.Variable(filter_data)
 x = tf.placeholder(tf.float32, shape=[1, None, None, 3], name='dnn_in')
-y = tf.nn.conv2d(x, filter, strides=[1, 1, 1, 1], padding='VALID', name='dnn_out')
+z1 = 0.039 + x
+z2 = x + 0.042
+z3 = z1 + z2
+z4 = z3 - 0.381
+z5 = z4 - x
+y = tf.math.maximum(z5, 0.0, name='dnn_out')
 
 sess=tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -21,6 +24,4 @@ print("image_process.pb generated, please use \
 path_to_ffmpeg/tools/python/convert.py to generate image_process.model\n")
 
 output = sess.run(y, feed_dict={x: in_data})
-output = output * 255.0
-output = output.astype(np.uint8)
 imageio.imsave("out.jpg", np.squeeze(output))
